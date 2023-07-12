69c3b4bfdb28ccf78d7b332cfc2d61780ad76932
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-12 12:13:47 -0300

    feat: basic download

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("ok")
+from flask import (Flask, send_file, request, render_template)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/download', methods=['GET'])
+def download():
+    image_file = request.args.to_dict().get('image_file')
+    return send_file(f'static/{image_file}.png', as_attachment=True)
