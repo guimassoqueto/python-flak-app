8f328cf4b90a6a5a80bce97dab68e2d9007800bf
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-12 14:28:40 -0300

    feat: basic web app completed

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import (Flask, send_file, request, render_template)
+from os import listdir
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    images = sorted([f for f in listdir('static')])
+    return render_template('index.html', images=images)
 
 
 @app.route('/download', methods=['GET'])
 def download():
     image_file = request.args.to_dict().get('image_file')
-    return send_file(f'static/{image_file}.png', as_attachment=True)
+    return send_file(f'static/{image_file}', as_attachment=True)
