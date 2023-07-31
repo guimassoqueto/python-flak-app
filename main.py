1c0625c2371ec46db9cd6529eefd9e3c45ceeca5
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-31 15:59:42 -0300

    feat: update main, template

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import (Flask, send_file, request, render_template)
 from os import listdir
+from app.settings import FLASK_PORT, FLASK_HOST
+
 
 app = Flask(__name__)
 
@@ -14,3 +16,7 @@ def index():
 def download():
     image_file = request.args.to_dict().get('image_file')
     return send_file(f'static/{image_file}', as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(host=FLASK_HOST, port=FLASK_PORT)
