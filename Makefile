546dc521f09ca3081da8404595619f12dc3f23e0
guimassoqueto <gui.massoqueto@gmail.com>
2023-08-02 15:35:51 -0300

    feat: release 2.0.2

--- Makefile
+++ Makefile
@@ -4,4 +4,7 @@ env:
 
 # INIT APPLICATION
 a:
-	echo 123 1> static/123.txt && rm static/* && poetry run python main.py
+	echo 123 1> static/fofinho/123.txt && rm static/fofinho/* && echo 123 1> static/thunder/123.txt && rm static/thunder/* && poetry run python main.py
+
+f:
+	poetry run python main.py
--- main.py
+++ main.py
@@ -5,17 +5,33 @@ from app.settings import FLASK_PORT, FLASK_HOST
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    images = sorted([f for f in listdir('static')])
-    return render_template('index.html', images=images)
+    return '<a href="/thunder"><h3>Thunder</h3></a><a href="/fofinho"><h3>Fofinho</h3></a>'
 
 
-@app.route('/download', methods=['GET'])
-def download():
+@app.route('/thunder')
+def thunder():
+    images = [f for f in listdir('static/thunder')]
+    return render_template('thunder.html', images=images)
+
+@app.route('/thunder-download', methods=['GET'])
+def thunder_download():
     image_file = request.args.to_dict().get('image_file')
-    return send_file(f'static/{image_file}', as_attachment=True)
+    return send_file(f'static/thunder/{image_file}', as_attachment=True)
+
+
+
+@app.route('/fofinho')
+def fofinho():
+    images = [f for f in listdir('static/fofinho')]
+    return render_template('fofinho.html', images=images)
+
+
+@app.route('/fofinho-download', methods=['GET'])
+def fofinho_download():
+    image_file = request.args.to_dict().get('image_file')
+    return send_file(f'static/fofinho/{image_file}', as_attachment=True)
 
 
 if __name__ == "__main__":
